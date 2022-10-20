e7ee59ac25c50ceec995739584a2598549406eab
yhsiong <yhsiong@gmail.com>
2022-10-20 16:33:13 +0800

    commit

---
+++ Job Scheduling/Controllers/MaterialController.cs
@@ -0,0 +1,89 @@
+﻿using Dapper;
+using Job_Scheduling.Database;
+using Job_Scheduling.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace Job_Scheduling.Controllers
+{
+    public class MaterialController : Controller
+    {
+        private Material_Context _Material_Context;
+        private string _connStr = string.Empty;
+        private readonly ILogger<MaterialController> _logger;
+        public MaterialController(Material_Context Material_Context, ILogger<MaterialController> logger, IConfiguration configuration)
+        {
+            _Material_Context = Material_Context;
+            _logger = logger;
+            _connStr = configuration.GetConnectionString("DefaultConnection");
+        }
+        [HttpGet]
+        [Route("materials")]
+        public async Task<IActionResult> getMaterials()
+        {
+            List<Material.Dto.Get> materials = await Material.Operations.ReadAll(_Material_Context);
+            if (materials == null)
+            {
+                return StatusCode(404, string.Format("Could not find config"));
+            }
+            else
+            {
+                return StatusCode(200, materials);
+            }              
+        }
+
+        [HttpGet]
+        [Route("getMaterialById")]
+        public async Task<IActionResult> getMaterialById(Guid material_id)
+        {
+
+            Material.Dto.Get material = await Material.Operations.ReadSingleById(_Material_Context, material_id);
+            if (material == null)
+            {
+                return StatusCode(404, string.Format("Could not find config"));
+            }
+            else
+            {
+                return StatusCode(200, material);
+            } 
+        } 
+        [HttpPost]
+        [Route("material")]
+        public async Task<IActionResult> insertMaterial(Material.Dto.Post material)
+        {
+            material.material_id = new Guid();
+            material.material_created_at = DateTime.Now;
+            bool status = await Material.Operations.Create(_Material_Context, material);
+            if (status)
+            {
+                return StatusCode(200, material);
+            }
+            else
+            {
+                return StatusCode(404, string.Format("Could not find config"));
+            } 
+        }
+  
+        [HttpPut]
+        [Route("material")]
+        public async Task<IActionResult> updateMaterial(Material.Dto.Put material)
+        {
+            material.material_updated_at = DateTime.Now;
+            bool status = await Material.Operations.Update(_Material_Context, material);
+
+            if (status)
+            {
+                return StatusCode(200, material);
+            }
+            else
+            {
+                return StatusCode(404, string.Format("Could not find config"));
+            } 
+        }
+         
+    }
+}
---
+++ Job Scheduling/Controllers/ToolController.cs
@@ -0,0 +1,90 @@
+﻿using Dapper;
+using Job_Scheduling.Database;
+using Job_Scheduling.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace Job_Scheduling.Controllers
+{
+    public class ToolController : Controller
+    {
+        private Tool_Context _Tool_Context;
+        private string _connStr = string.Empty;
+        private readonly ILogger<ToolController> _logger;
+        public ToolController(Tool_Context Tool_Context, ILogger<ToolController> logger, IConfiguration configuration)
+        {
+            _Tool_Context = Tool_Context;
+            _logger = logger;
+            _connStr = configuration.GetConnectionString("DefaultConnection");
+        }
+        [HttpGet]
+        [Route("tools")]
+        public async Task<IActionResult> getTools()
+        {
+            List<Tool.Dto.Get> tools = await Tool.Operations.ReadAll(_Tool_Context);
+            if (tools == null)
+            {
+                return StatusCode(404, string.Format("Could not find config"));
+            }
+            else
+            {
+                return StatusCode(200, tools);
+            }              
+        }
+
+        [HttpGet]
+        [Route("getToolById")]
+        public async Task<IActionResult> getToolById(Guid tool_id)
+        {
+
+            Tool.Dto.Get tool = await Tool.Operations.ReadSingleById(_Tool_Context, tool_id);
+            if (tool == null)
+            {
+                return StatusCode(404, string.Format("Could not find config"));
+            }
+            else
+            {
+                return StatusCode(200, tool);
+            } 
+        }
+
+        [HttpPost]
+        [Route("tool")]
+        public async Task<IActionResult> insertTool(Tool.Dto.Post tool)
+        {
+            tool.tool_id = new Guid();
+            tool.tool_created_at = DateTime.Now;
+            bool status = await Tool.Operations.Create(_Tool_Context, tool);
+            if (status)
+            {
+                return StatusCode(200, tool);
+            }
+            else
+            {
+                return StatusCode(404, string.Format("Could not find config"));
+            } 
+        }
+  
+        [HttpPut]
+        [Route("tool")]
+        public async Task<IActionResult> updateVehicle(Tool.Dto.Put tool)
+        {
+            tool.tool_updated_at = DateTime.Now;
+            bool status = await Tool.Operations.Update(_Tool_Context, tool);
+
+            if (status)
+            {
+                return StatusCode(200, tool);
+            }
+            else
+            {
+                return StatusCode(404, string.Format("Could not find config"));
+            } 
+        }
+         
+    }
+}
